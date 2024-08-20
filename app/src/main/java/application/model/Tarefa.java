f7a7aae556c18fd524b78ead614cd1746e255824
MatheusMorandi <theu.morandi@gmail.com>
2024-08-20 18:20:39 +0000

    Primeira Parte

---
+++ app/src/main/java/application/model/Tarefa.java
@@ -0,0 +1,51 @@
+package application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tarefas")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(nullable = false)
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Column(nullable = false)
+    private boolean concluido;
+
+    public boolean getConcluido() {
+        return concluido;
+    }
+
+    public void setConcluido(boolean concluido) {
+        this.concluido = concluido;
+    }
+
+    
+
+    
+}
